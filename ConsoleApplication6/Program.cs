93b1d2fdb74e6c40f76b22313276f549947a128a
Brian Wood <uoflcardinalsalltheway@yahoo.com>
2017-04-23 16:35:30 -0400

    Star Box

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Please enter an integer for the size of your star box: ");
+            input = int.Parse(Console.ReadLine());
+            PrintStars(input);
+        }
+        public static void PrintStars(int input)
+        {
+            string star = "*";
+
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j < input; j++)
+                {
+                    Console.Write(star);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
